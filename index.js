f48f39e869f8be0c8f1ec82ad99d45425aae986e
Jean Carlo Sinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-07 06:25:38 -0500

    Update index.js
    refactor of this code to make it shorter and simple, reducing redundancy and making a better readability

--- index.js
+++ index.js
@@ -1,81 +1,42 @@
-var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i<numberOfDrumsButtons; i++) {
-
-// Detecting Button press
-
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-       var buttonInnerHTML = this.innerHTML;
-
-       makeSound(buttonInnerHTML);
-
-       buttonAnimation(buttonInnerHTML);
-
-      });
-   }
-
-// Detecting Keyboard press
-
-document.addEventListener("keydown", function(event) {
-
-   makeSound(event.key);
-
-   buttonAnimation(event.key);
-
-});
-
-function makeSound(key) {
-
-      switch (key) {
-        case "w":
-           var tom4 = new Audio("sounds/tom-4.mp3");
-           tom4.play();
-           break;
-
-        case "a":
-           var tom3 = new Audio("sounds/tom-3.mp3");
-           tom3.play();
-           break;
-
-        case "s":
-           var tom2 = new Audio("sounds/tom-2.mp3");
-           tom2.play();
-           break;
-
-        case "d":
-           var tom1 = new Audio("sounds/tom-1.mp3");
-           tom1.play();
-           break;
-
-        case "j":
-           var snare = new Audio("sounds/snare.mp3");
-           snare.play();
-           break;
-        
-        case "k":
-           var kick = new Audio("sounds/kick-bass.mp3");
-           kick.play();
-           break;
-
-        case "l":
-           var crash = new Audio("sounds/crash.mp3");
-           crash.play();
-           break;
-
-        default:
-
-       }
-    // what to do when click detected.
+// Create an object to map keys to sound files
+const sounds = {
+  w: 'sounds/tom-4.mp3',
+  a: 'sounds/tom-3.mp3',
+  s: 'sounds/tom-2.mp3',
+  d: 'sounds/tom-1.mp3',
+  j: 'sounds/snare.mp3',
+  k: 'sounds/kick-bass.mp3',
+  l: 'sounds/crash.mp3'
 };
 
-function buttonAnimation(currentKey){
-   
-   var activeButton = document.querySelector("." + currentKey);
-   
-   activeButton.classList.add("pressed");
-   
-   setTimeout(function() {
-      activeButton.classList.remove("pressed");
-   }, 140);
-}
+// Select all drums and add event listeners
+document.querySelectorAll(".drum").forEach(drum => {
+  drum.addEventListener("click", function() {
+    let key = this.innerHTML;
+    playSound(key);
+    animateButton(key);
+  });
+});
+
+// Add keydown event listener
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+  animateButton(event.key);
+});
+
+// Function to play sound
+function playSound(key) {
+  if (sounds[key]) {
+    let audio = new Audio(sounds[key]);
+    audio.play();
+  }
+};
+
+// Function to animate button
+function animateButton(key) {
+  let activeButton = document.querySelector("." + key);
+  if (activeButton) {
+    activeButton.classList.add("pressed");
+    setTimeout(() => activeButton.classList.remove("pressed"), 140);
+  }
+};
